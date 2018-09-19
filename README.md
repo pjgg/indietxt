5e27e5e1103ae1267cdafc817f6aa2498d77961e
Pablo Jose Gonzalez Granados <pjgg@Pablos-MacBook-Pro.local>
2018-09-19 13:25:30 +0200

    [Test] performance test

--- README.md
+++ README.md
@@ -25,8 +25,8 @@ On the other hand we encourage you to develop your own datasets. You could place
 
 ```
 .
-├── GetOneItemSize.png
 ├── README.md
+├── complexityChart.png
 ├── logo.png
 ├── pom.xml
 ├── src
@@ -78,12 +78,37 @@ On the other hand we encourage you to develop your own datasets. You could place
 │   │                   ├── StockEntryDAO.java
 │   │                   └── StockEntryPredicates.java
 │   ├── microbenchmark
-│   │   └── java
-│   │       └── org
-│   │           └── indietxt
-│   │               └── performance
-│   │                   ├── BenchmarkRunner.java
-│   │                   └── StockManagerPerformance.java
+│   │   ├── java
+│   │   │   └── org
+│   │   │       └── indietxt
+│   │   │           └── performance
+│   │   │               ├── BenchmarkRunner.java
+│   │   │               └── StockManagerPerformance.java
+│   │   └── resources
+│   │       ├── loadProductSizeEighHundredItems.csv
+│   │       ├── loadProductSizeFiftyItems.csv
+│   │       ├── loadProductSizeFiveHundredItems.csv
+│   │       ├── loadProductSizeFourHundredItems.csv
+│   │       ├── loadProductSizeHundredItems.csv
+│   │       ├── loadProductSizeNineHundredItems.csv
+│   │       ├── loadProductSizeSevenHundredItems.csv
+│   │       ├── loadProductSizeSixHundredItems.csv
+│   │       ├── loadProductSizeTenItems.csv
+│   │       ├── loadProductSizeThousandItems.csv
+│   │       ├── loadProductSizeThreeHundredItems.csv
+│   │       ├── loadProductSizeTwoHundredItems.csv
+│   │       ├── loadStockEighHundredItems.csv
+│   │       ├── loadStockFiftyItems.csv
+│   │       ├── loadStockFiveHundredItems.csv
+│   │       ├── loadStockFourHundredItems.csv
+│   │       ├── loadStockHundredItems.csv
+│   │       ├── loadStockNineHundredItems.csv
+│   │       ├── loadStockSevenHundredItems.csv
+│   │       ├── loadStockSixHundredItems.csv
+│   │       ├── loadStockTenItems.csv
+│   │       ├── loadStockThousandItems.csv
+│   │       ├── loadStockThreeHundredItems.csv
+│   │       └── loadStockTwoHundredItems.csv
 │   └── test
 │       └── java
 │           └── org
@@ -95,7 +120,6 @@ On the other hand we encourage you to develop your own datasets. You could place
 │                       ├── ProductSizeDaoTest.java
 │                       └── StockEntryDaoTest.java
 └── suply-chain.iml
-
 ```
 
 
@@ -119,17 +143,182 @@ The goal is to provide us with a full understanding of your coding style and ski
 
 ## Microbenchmark
 
-With just one item in stock and product storage, I request to retrieve the stock of each item(with equivalent logic - full exercise), and I got the following result.
+I request to retrieve the stock of each item(with equivalent logic - full exercise), and I got the following result.
 We run, 2 Rounds, 20 iterations each one.
 
+<img align="center" src="https://github.com/pjgg/indietxt/blob/master/complexityChart.png">
+
+
+Summary
+     
+   ```
+   Benchmark                                              (iterations)  Mode  Cnt   Score   Error  Units
+   
+  StockManagerPerformance.stockManagerTest                              1  avgt   20   0.006 ± 0.001  ms/op
+  StockManagerPerformance.stockManagerTenItemsTest                      1  avgt   20   0.016 ± 0.001  ms/op
+  StockManagerPerformance.stockManagerFiftyItemsTest                    1  avgt   20   0.140 ± 0.006  ms/op
+  StockManagerPerformance.stockManagerHundredItemsTest                  1  avgt   20   0.313 ± 0.014  ms/op
+  StockManagerPerformance.stockManagerTwoHundredItemsTest               1  avgt   20   0.905 ± 0.013  ms/op  
+  StockManagerPerformance.stockManagerThreeHundredItemsTest             1  avgt   20   1.806 ± 0.044  ms/op
+  StockManagerPerformance.stockManagerFourHundredItemsTest              1  avgt   20   3.077 ± 0.139  ms/op
+  StockManagerPerformance.stockManagerFiveHundredItemsTest              1  avgt   20   4.550 ± 0.031  ms/op
+  StockManagerPerformance.stockManagerSixHundredItemsTest               1  avgt   20   6.684 ± 0.165  ms/op
+  StockManagerPerformance.stockManagerSevenHundredItemsTest             1  avgt   20   8.918 ± 0.173  ms/op
+  StockManagerPerformance.stockManagerEighHundredItemsTest              1  avgt   20  12.289 ± 0.343  ms/op
+  StockManagerPerformance.stockManagerNineHundredItemsTest              1  avgt   20  15.159 ± 0.759  ms/op
+  StockManagerPerformance.stockManagerThousandItemsTest                 1  avgt   20  17.997 ± 0.246  ms/op
+   ```
+
+ 1 items Test (product / stock)
+ 
 ```
 Result "org.indietxt.performance.StockManagerPerformance.stockManagerTest":
-  0.560 ±(99.9%) 0.116 ms/op [Average]
-  (min, avg, max) = (0.318, 0.560, 0.737), stdev = 0.133
-  CI (99.9%): [0.444, 0.676] (assumes normal distribution)
+  0.006 ±(99.9%) 0.001 ms/op [Average]
+  (min, avg, max) = (0.004, 0.006, 0.008), stdev = 0.001
+  CI (99.9%): [0.005, 0.006] (assumes normal distribution)
   ```
   
-So, all executions with a 99.9% will be between 0.444 ms and 0.676 ms
-<img align="center" src="https://github.com/pjgg/indietxt/blob/master/GetOneItemSize.png">
+  10 items Test (product / stock)
+  
+  ```
+ Result "org.indietxt.performance.StockManagerPerformance.stockManagerTenItemsTest":
+   0.016 ±(99.9%) 0.001 ms/op [Average]
+   (min, avg, max) = (0.015, 0.016, 0.017), stdev = 0.001
+   CI (99.9%): [0.015, 0.016] (assumes normal distribution)
+  ```
+    
+  50 items Test (product / stock)
+  
+  ```
+Result "org.indietxt.performance.StockManagerPerformance.stockManagerFiftyItemsTest":
+  0.140 ±(99.9%) 0.006 ms/op [Average]
+  (min, avg, max) = (0.134, 0.140, 0.162), stdev = 0.007
+  CI (99.9%): [0.133, 0.146] (assumes normal distribution)
+  ```
+    
+   100 items Test (product / stock)
+   
+  ```
+  Result "org.indietxt.performance.StockManagerPerformance.stockManagerHundredItemsTest":
+    0.313 ±(99.9%) 0.014 ms/op [Average]
+    (min, avg, max) = (0.301, 0.313, 0.357), stdev = 0.016
+    CI (99.9%): [0.299, 0.326] (assumes normal distribution)
+  ```
+  
+   200 items Test (product / stock)
+     
+    ```
+    Result "org.indietxt.performance.StockManagerPerformance.stockManagerTwoHundredItemsTest":
+      0.905 ±(99.9%) 0.013 ms/op [Average]
+      (min, avg, max) = (0.873, 0.905, 0.929), stdev = 0.015
+      CI (99.9%): [0.891, 0.918] (assumes normal distribution)
+    ```
+  
+   300 items Test (product / stock)
+     
+    ```
+    Result "org.indietxt.performance.StockManagerPerformance.stockManagerThreeHundredItemsTest":
+      1.806 ±(99.9%) 0.044 ms/op [Average]
+      (min, avg, max) = (1.722, 1.806, 1.913), stdev = 0.050
+      CI (99.9%): [1.762, 1.850] (assumes normal distribution)
+    ```
+ 
+   400 items Test (product / stock)
+         
+     ```
+     Result "org.indietxt.performance.StockManagerPerformance.stockManagerFourHundredItemsTest":
+       3.077 ±(99.9%) 0.139 ms/op [Average]
+       (min, avg, max) = (2.972, 3.077, 3.536), stdev = 0.160
+       CI (99.9%): [2.938, 3.216] (assumes normal distribution)
+     ```
+     
+   500 items Test (product / stock)
+        
+    ```
+    Result "org.indietxt.performance.StockManagerPerformance.stockManagerFiveHundredItemsTest":
+      4.550 ±(99.9%) 0.031 ms/op [Average]
+      (min, avg, max) = (4.491, 4.550, 4.656), stdev = 0.036
+      CI (99.9%): [4.519, 4.581] (assumes normal distribution)
+    ```
+  
+  600 items Test (product / stock)
+           
+       ```
+     Result "org.indietxt.performance.StockManagerPerformance.stockManagerSixHundredItemsTest":
+       6.684 ±(99.9%) 0.165 ms/op [Average]
+       (min, avg, max) = (6.576, 6.684, 7.394), stdev = 0.190
+       CI (99.9%): [6.519, 6.849] (assumes normal distribution)
+       ```
+       
+   700 items Test (product / stock)
+         
+     ```
+    Result "org.indietxt.performance.StockManagerPerformance.stockManagerSevenHundredItemsTest":
+      8.918 ±(99.9%) 0.173 ms/op [Average]
+      (min, avg, max) = (8.719, 8.918, 9.522), stdev = 0.199
+      CI (99.9%): [8.745, 9.091] (assumes normal distribution)
+     ```
+          
+   800 items Test (product / stock)
+       
+   ```
+   Result "org.indietxt.performance.StockManagerPerformance.stockManagerEighHundredItemsTest":
+     12.289 ±(99.9%) 0.343 ms/op [Average]
+     (min, avg, max) = (11.650, 12.289, 13.479), stdev = 0.395
+     CI (99.9%): [11.945, 12.632] (assumes normal distribution)
+   ```
+      
+   900 items Test (product / stock)
+           
+    ```
+     Result "org.indietxt.performance.StockManagerPerformance.stockManagerNineHundredItemsTest":
+       15.159 ±(99.9%) 0.759 ms/op [Average]
+       (min, avg, max) = (14.204, 15.159, 17.892), stdev = 0.874
+       CI (99.9%): [14.401, 15.918] (assumes normal distribution)
+    ```
+       
+   1000 items Test (product / stock)
+   
+   ```
+  Result "org.indietxt.performance.StockManagerPerformance.stockManagerThousandItemsTest":
+    17.997 ±(99.9%) 0.246 ms/op [Average]
+    (min, avg, max) = (17.761, 17.997, 18.681), stdev = 0.283
+    CI (99.9%): [17.752, 18.243] (assumes normal distribution)
+   ```
+   
+## Conclusion
 
+Based on our studies we concluded that the current basic implementation [StockManagerReferenceImpl](https://github.com/pjgg/indietxt/blob/master/src/main/java/org/indietxt/service/impl/StockManagerReferenceImpl.java) has a exponential cyclomatic complexity level. On the first hand it's quite surprising if you have a look the method code, because if you consider a stream as a loop, then I will say that the complexity level should be 3 * O(n). 
+
+Code snippet:
+  ```
+  @Override
+  	public List<StockEntry> retrieveStock() {
+  
+  		List<ProductSize> pSize =  productSizeDAO.filterEntities(ProductSizePredicates.alwaysTrue()).collect(Collectors.toList());
+  		List<Integer> ids = pSize.stream().map(ProductSize::getId).collect(Collectors.toList());
+  		return stockEntryDAO.filterEntities(StockEntryPredicates.byIds(ids.toArray(new Integer[ids.size()]))).collect(Collectors.toList());
+  	}
+  ```
+  
+But the data never lie you (this could be the title of a movie ;) ). 
+This exercise it's just an example of Java8 features, however if you think to move on "this code" to production you should analise the behavior with a profiler in order to understand, where the bottle neck is?. Could be memory, could be CPU... etc. 
+
+Finally an other conclusion could be that nowadays with lambdas, static imports or default methods that inject code into your class make more fuzzy your code, in order to understand your "cyclomatic complexity" this is why is always a good reason to test your code with frameworks or libraries that "
+throw a little light on your darkness". Also the numbers are not important at all (depends on your laptop, the services or programs that your are running at this moment), however the shape of your graph it's very important.
+
+Well Thank you if you are following this exercise, and remember that you could reuse the acceptance and performance test defined in this project with your own [StockManager](https://github.com/pjgg/indietxt/blob/master/src/main/java/org/indietxt/service/StockManager.java) implementation. Just remember to add your implementation to the [ServiceLocator](https://github.com/pjgg/indietxt/blob/master/src/main/java/org/indietxt/service/StockManagerServiceLocator.java) and also update your acceptance test [given](https://github.com/pjgg/indietxt/blob/master/src/acceptance-test/java/org/indietxt/acceptanceTest/CalculateStockIT.java) step in order to add your "algorithm" name.  
+
+```
+switch(algorithm) {
+            case "yourAlgorithmName"
+			default:
+				stockManagerSelected = stockManagerReference;
+				break;
+		}
+```
+
+Finally you need to update the [acceptance test definition](https://github.com/pjgg/indietxt/blob/master/src/acceptance-test/resources/features/ExerciseAcceptance.feature) and replace the keyword "ReferenceImpl" by your algorithm name. By this way you will set your implementation as a default acceptance test implementation.
+
+Well if you have any question, or you just wanna chat about technology, you can contact me at anytime. 
   
--- src/microbenchmark/java/org/indietxt/performance/StockManagerPerformance.java
+++ src/microbenchmark/java/org/indietxt/performance/StockManagerPerformance.java
@@ -1,9 +1,11 @@
 package org.indietxt.performance;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.indietxt.acceptanceTest.utils.CsvFileReader;
 import org.indietxt.model.ProductSize;
 import org.indietxt.model.StockEntry;
 import org.indietxt.service.StockManager;
@@ -26,10 +28,49 @@ public class StockManagerPerformance {
 	@State(Scope.Benchmark)
 	public static class ExecutionPlan {
 
+		private final ClassLoader classLoader = getClass().getClassLoader();
+
 		@Param({ "1"})
 		public int iterations;
 
-		ProductSize productSizeExample;
+		List<ProductSize> productSizeOneItemExample;
+		List<StockEntry> stockOneItemExample;
+
+		List<ProductSize> productSizeTenItemExample;
+		List<StockEntry> stockTenItemExample;
+
+		List<ProductSize> productSizeFiftytemExample;
+		List<StockEntry> stockFiftyItemExample;
+
+		List<ProductSize> productSizeHundredItemExample;
+		List<StockEntry> stockHundredItemExample;
+
+		List<ProductSize> productSizeTwoHundredItemExample;
+		List<StockEntry> stockTwoHundredItemExample;
+
+		List<ProductSize> productSizeThreeHundredItemExample;
+		List<StockEntry> stockThreeHundredItemExample;
+
+		List<ProductSize> productSizeFourHundredItemExample;
+		List<StockEntry> stockFourHundredItemExample;
+
+		List<ProductSize> productSizeFiveHundredItemExample;
+		List<StockEntry> stockFiveHundredItemExample;
+
+		List<ProductSize> productSizeSixHundredItemExample;
+		List<StockEntry> stockSixHundredItemExample;
+
+		List<ProductSize> productSizeSevenHundredItemExample;
+		List<StockEntry> stockSevenHundredItemExample;
+
+		List<ProductSize> productSizeEighHundredItemExample;
+		List<StockEntry> stockEighHundredItemExample;
+
+		List<ProductSize> productSizeNineHundredItemExample;
+		List<StockEntry> stockNineHundredItemExample;
+
+		List<ProductSize> productSizeThousandItemExample;
+		List<StockEntry> stockThousandItemExample;
 
 		StockManager stockManager;
 
@@ -41,12 +82,107 @@ public class StockManagerPerformance {
 			stockManager.cleanProductSize();
 
 			ProductSize productSize = new ProductSize(10,101, "XL");
-			List<ProductSize> streamProductSize = Arrays.asList(productSize);
+			productSizeOneItemExample = Arrays.asList(productSize);
 
 			StockEntry stockEntry = new StockEntry(10,3);
-			List<StockEntry> streamStockEntry = Arrays.asList(stockEntry);
+			stockOneItemExample = Arrays.asList(stockEntry);
 
-			stockManager.loadItems(streamProductSize, streamStockEntry);
+			CsvFileReader<ProductSize> csvReader = new CsvFileReader();
+			File productSizeCsv = new File(classLoader.getResource("loadProductSizeTenItems.csv").getFile());
+			productSizeTenItemExample = csvReader.read(productSizeCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvStockReader = new CsvFileReader();
+			File stockEntryCsv = new File(classLoader.getResource("loadStockTenItems.csv").getFile());
+			stockTenItemExample = csvStockReader.read(stockEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvFiftyReader = new CsvFileReader();
+			File productSizeFiftyCsv = new File(classLoader.getResource("loadProductSizeFiftyItems.csv").getFile());
+			productSizeFiftytemExample = csvFiftyReader.read(productSizeFiftyCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry>  csvStockFiftyReader = new CsvFileReader();
+			File stockFiftyEntryCsv = new File(classLoader.getResource("loadStockFiftyItems.csv").getFile());
+			stockFiftyItemExample = csvStockFiftyReader.read(stockFiftyEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvHundredReader = new CsvFileReader();
+			File productSizeHundredCsv = new File(classLoader.getResource("loadProductSizeHundredItems.csv").getFile());
+			productSizeHundredItemExample = csvHundredReader.read(productSizeHundredCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvHundredStockReader = new CsvFileReader();
+			File stockHundredEntryCsv = new File(classLoader.getResource("loadStockHundredItems.csv").getFile());
+			stockHundredItemExample = csvHundredStockReader.read(stockHundredEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvTwoHundredReader = new CsvFileReader();
+			File productSizecsvTwoHundredCsv = new File(classLoader.getResource("loadProductSizeTwoHundredItems.csv").getFile());
+			productSizeTwoHundredItemExample = csvTwoHundredReader.read(productSizecsvTwoHundredCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvTwoHundredStockReader = new CsvFileReader();
+			File stockTwoHundredEntryCsv = new File(classLoader.getResource("loadStockTwoHundredItems.csv").getFile());
+			stockTwoHundredItemExample = csvTwoHundredStockReader.read(stockTwoHundredEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvThreeHundredReader = new CsvFileReader();
+			File productSizecsvThreeHundredCsv = new File(classLoader.getResource("loadProductSizeThreeHundredItems.csv").getFile());
+			productSizeThreeHundredItemExample = csvThreeHundredReader.read(productSizecsvThreeHundredCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvThreeHundredStockReader = new CsvFileReader();
+			File stockThreeHundredEntryCsv = new File(classLoader.getResource("loadStockThreeHundredItems.csv").getFile());
+			stockThreeHundredItemExample = csvThreeHundredStockReader.read(stockThreeHundredEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvFourHundredReader = new CsvFileReader();
+			File productSizecsvFourHundredCsv = new File(classLoader.getResource("loadProductSizeFourHundredItems.csv").getFile());
+			productSizeFourHundredItemExample = csvFourHundredReader.read(productSizecsvFourHundredCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvFourHundredStockReader = new CsvFileReader();
+			File stockFourHundredEntryCsv = new File(classLoader.getResource("loadStockFourHundredItems.csv").getFile());
+			stockFourHundredItemExample = csvFourHundredStockReader.read(stockFourHundredEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvFiveHundredReader = new CsvFileReader();
+			File productSizecsvFiveHundredCsv = new File(classLoader.getResource("loadProductSizeFiveHundredItems.csv").getFile());
+			productSizeFiveHundredItemExample = csvFiveHundredReader.read(productSizecsvFiveHundredCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvFiveHundredStockReader = new CsvFileReader();
+			File stockFiveHundredEntryCsv = new File(classLoader.getResource("loadStockFiveHundredItems.csv").getFile());
+			stockFiveHundredItemExample = csvFiveHundredStockReader.read(stockFiveHundredEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvSixHundredReader = new CsvFileReader();
+			File productSizecsvSixHundredCsv = new File(classLoader.getResource("loadProductSizeSixHundredItems.csv").getFile());
+			productSizeSixHundredItemExample = csvSixHundredReader.read(productSizecsvSixHundredCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvSixHundredStockReader = new CsvFileReader();
+			File stockSixHundredEntryCsv = new File(classLoader.getResource("loadStockSixHundredItems.csv").getFile());
+			stockSixHundredItemExample = csvSixHundredStockReader.read(stockSixHundredEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvSevenHundredReader = new CsvFileReader();
+			File productSizecsvSevenHundredCsv = new File(classLoader.getResource("loadProductSizeSevenHundredItems.csv").getFile());
+			productSizeSevenHundredItemExample = csvSevenHundredReader.read(productSizecsvSevenHundredCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvSevenHundredStockReader = new CsvFileReader();
+			File stockSevenHundredEntryCsv = new File(classLoader.getResource("loadStockSevenHundredItems.csv").getFile());
+			stockSevenHundredItemExample = csvSevenHundredStockReader.read(stockSevenHundredEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvEighHundredReader = new CsvFileReader();
+			File productSizecsvEighHundredCsv = new File(classLoader.getResource("loadProductSizeEighHundredItems.csv").getFile());
+			productSizeEighHundredItemExample = csvEighHundredReader.read(productSizecsvEighHundredCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvEighHundredStockReader = new CsvFileReader();
+			File stockEighHundredEntryCsv = new File(classLoader.getResource("loadStockEighHundredItems.csv").getFile());
+			stockEighHundredItemExample = csvEighHundredStockReader.read(stockEighHundredEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+
+			CsvFileReader<ProductSize> csvNineHundredReader = new CsvFileReader();
+			File productSizecsvNineHundredCsv = new File(classLoader.getResource("loadProductSizeNineHundredItems.csv").getFile());
+			productSizeNineHundredItemExample = csvNineHundredReader.read(productSizecsvNineHundredCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvNineHundredStockReader = new CsvFileReader();
+			File stockNineHundredEntryCsv = new File(classLoader.getResource("loadStockNineHundredItems.csv").getFile());
+			stockNineHundredItemExample = csvNineHundredStockReader.read(stockNineHundredEntryCsv, new StockEntry(0,0), StockEntry.class);
+
+			CsvFileReader<ProductSize> csvThousandReader = new CsvFileReader();
+			File productSizeThousandCsv = new File(classLoader.getResource("loadProductSizeThousandItems.csv").getFile());
+			productSizeThousandItemExample = csvThousandReader.read(productSizeThousandCsv, new ProductSize(0,0, "desc"), ProductSize.class);
+
+			CsvFileReader<StockEntry> csvThousandStockReader = new CsvFileReader();
+			File stockThousandEntryCsv = new File(classLoader.getResource("loadStockThousandItems.csv").getFile());
+			stockThousandItemExample = csvThousandStockReader.read(stockThousandEntryCsv, new StockEntry(0,0), StockEntry.class);
 
 		}
 	}
@@ -57,10 +193,157 @@ public class StockManagerPerformance {
 	@OutputTimeUnit(TimeUnit.MILLISECONDS)
 	@Warmup(iterations = 5)
 	public void stockManagerTest(ExecutionPlan plan) {
-
+		plan.stockManager.loadItems(plan.productSizeOneItemExample, plan.stockOneItemExample);
 		for (int i = plan.iterations; i > 0; i--) {
 			plan.stockManager.retrieveStock();
 		}
 	}
 
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerTenItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeTenItemExample, plan.stockTenItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerFiftyItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeFiftytemExample, plan.stockFiftyItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerHundredItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeHundredItemExample, plan.stockHundredItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerThousandItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeThousandItemExample, plan.stockThousandItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerTwoHundredItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeTwoHundredItemExample, plan.stockTwoHundredItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerThreeHundredItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeThreeHundredItemExample, plan.stockThreeHundredItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerFourHundredItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeFourHundredItemExample, plan.stockFourHundredItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerFiveHundredItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeFiveHundredItemExample, plan.stockFiveHundredItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerSixHundredItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeSixHundredItemExample, plan.stockSixHundredItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerSevenHundredItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeSevenHundredItemExample, plan.stockSevenHundredItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerEighHundredItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeEighHundredItemExample, plan.stockEighHundredItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
+
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5)
+	public void stockManagerNineHundredItemsTest(ExecutionPlan plan) {
+		plan.stockManager.loadItems(plan.productSizeNineHundredItemExample, plan.stockNineHundredItemExample);
+		for (int i = plan.iterations; i > 0; i--) {
+			plan.stockManager.retrieveStock();
+		}
+	}
 }
